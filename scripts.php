45b0404ca5ea8f3202ca153471b9b29adb5b9742
rishi1157 <122249866+rishi1157@users.noreply.github.com>
2023-03-21 16:48:34 +0530

    No need

--- scripts.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-
-function my_plugin_style()
-{
-    wp_enqueue_style('my_css', plugin_dir_path( __FILE__ ) . 'admin/css/style.css');
-}
-add_action('admin_enqueue_scripts', 'my_plugin_style');
-
